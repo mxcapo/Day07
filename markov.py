0789b8c003643099fbddf82a1ce924e1b20e0549
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 16:28:15 -0700

    woohoo got the pairs for the dictionary

--- markov.py
+++ markov.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
 import sys
+from sys import argv
+
+script, filename = argv
+
+
+
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
-    return {}
+    cinderella_dict = {}
+    f = open(corpus)
+    i = 0
+
+    for line in f:
+        line = line.strip()
+        words = line.split() 
+        # return words
+       
+        for i in range(len(words) - 2):
+            dictionary_key_pair = (words[i], words[i + 1])
+            dictionary_value = [words[i+2]]
+            
+            if dictionary_key_pair not in cinderella_dict:
+                cinderella_dict[dictionary_key_pair] = dictionary_value
+            else:
+                cinderella_dict[dictionary_key_pair].append(dictionary_value)
+        # print words
+        return cinderella_dict       
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
@@ -16,7 +40,7 @@ def main():
     args = sys.argv
 
     # Change this to read input_text from a file
-    input_text = "Some text"
+    input_text = filename
 
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
