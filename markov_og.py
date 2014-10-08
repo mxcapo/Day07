4e7be29bad622162763a28cd9e074fa8519428d5
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 12:53:31 -0700

    working markov woohoo

--- markov_og.py
+++ markov_og.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-
+import random
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
@@ -11,7 +11,7 @@ def make_chains(corpus):
     text_block = f.read()
     stripped_block = text_block.strip()
     
-    punctuation = ['$', '%', '&', '(', ')', '*', '+', '/', '<', '=', '>', '@', '[', ']', '^', '`', '{', '|', '}', '~', '_']
+    punctuation = ['%', '&', '(', ')', '*', '+', '/', '<', '=', '>', '@', '[', ']', '^', '`', '{', '|', '}', '~', '_']
    
     block_no_punctuation = ""
     
@@ -38,15 +38,33 @@ def make_chains(corpus):
 
     return markov_dictionary 
 
-
-
-    # return {}
-    # print split_block
-
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
-    return "Here's some random text."
+    # return "Here's some random text."
+
+    start_key = random.choice(chains.keys()) # Still stunting,
+    value = random.choice(chains[start_key]) # how
+    silly_list = str(start_key[0]) + " " + str(start_key[1]) + " " # Still stunting,
+
+    # for i in range(len(chains.keys())):
+    #     print start_key
+    #     print value
+
+    while start_key in chains.keys():
+        # print "This is our start_key: %s, %s" % (start_key[0], start_key[1])
+        # print "This is our value: %s" % value
+        # markov_phrase = start_key[0],start_key[1], value,
+        # print value
+        value = random.choice(chains[start_key])
+        silly_list = silly_list + str(value) + " "
+        start_key = (start_key[1], value) # start key s/b stunting, how
+        if len(silly_list) > 100:
+            break
+        # silly_list.append(value,)
+        # print start_key
+
+    print silly_list
 
 def main():
     args = sys.argv
@@ -57,7 +75,7 @@ def main():
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
     # print random_text
-    print chain_dict
+    # print chain_dict
 
 if __name__ == "__main__":
     main()
