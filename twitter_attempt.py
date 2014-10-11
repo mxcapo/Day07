3d585876962d047614f40bb0343bebf9cf53c82a
Meredith Capone <meredithcapone@JKMBP.local>
2014-10-10 17:03:18 -0700

    twitter api accesses working

--- twitter_attempt.py
+++ twitter_attempt.py
@@ -5,3 +5,6 @@ api_key = os.environ.get('TWITTER_API_KEY')
 api_secret = os.environ.get('TWITTER_API_SECRET')
 access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
 access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+api = twitter.Api(consumer_key=os.environ.get('TWITTER_API_KEY'), consumer_secret=os.environ.get('TWITTER_API_SECRET'),
+    access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN'), access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
